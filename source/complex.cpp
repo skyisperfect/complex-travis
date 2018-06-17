de7aa9cee23f72d5bbf812a7844282b5aa8ecf14
skyisperfect <warrior2135@gmail.com>
2018-06-17 14:50:02 +0300

    trird test

--- source/complex.cpp
+++ source/complex.cpp
@@ -1,44 +1,45 @@
-#include <iostream>
-#include <cmath>
 #include "complex.hpp"
-using namespace std;
 
-class complex{
-private:
-	double Re, Im;
-public:
-	complex(){
+
+	complex::complex(){
 Re=0.0;
 Im=0.0; 
 }
-	complex(double RE, double IM){
+	complex::complex(double RE, double IM){
 Re=RE;
 Im=IM; 
 }
-	complex(const complex& sF){
+	complex::complex(const complex& sF){
 Re=sF.Re;
 Im=sF.Im;
 }
-	void print(ostream& print){
+	void complex::print(ostream& print){
 		print << Re << " + " << Im << "i" << endl; 
 	}
 	
-	void add(const complex& sF){
+	double complex::real(){
+    		return Re;
+		}
+	double complex::imaginary(){
+    	return Im;
+		}
+
+	void complex::add(const complex& sF){
 		Re += sF.Re;
 		Im += sF.Im;
 	}
 
-	void sub(const complex& sF){
+	void complex::sub(const complex& sF){
 		Re -= sF.Re;
 		Im -= sF.Im;
 	}
 
-	void mul(int M){
+	void complex::mul(int M){
 		Re *= M;
 		Im *= M;
 	}
 
-	void div(int D){
+	void complex::div(int D){
 	try{
 		if(D==0){
 			throw 1;
@@ -52,42 +53,42 @@ Im=sF.Im;
 }
 
 
- complex operator*(const complex& sF){
+ complex complex::operator*(const complex& sF){
         return complex(Re*sF.Re - Im*sF.Im, Im*sF.Re + Re*sF.Im);
     }
-    complex operator/(const complex& sF){
+    complex complex::operator/(const complex& sF){
         return complex((Re*sF.Re + Im*sF.Im)/(sF.Re*sF.Re + sF.Im*sF.Im),(-Re*sF.Im + Im*sF.Re)/(sF.Re*sF.Re + sF.Im*sF.Im));
     }
-    complex operator+=(const complex& sF){
+    complex complex::operator+=(const complex& sF){
         Re += sF.Re;
         Im += sF.Im;
         return complex(Re, Im);
     }
-    complex operator-=(const complex& sF){
+    complex complex::operator-=(const complex& sF){
         Re -= sF.Re;
         Im -= sF.Im;
         return complex(Re, Im);
     }
-    complex operator*=(const complex& sF){
+    complex complex::operator*=(const complex& sF){
         double  tmp;
         tmp = Re*sF.Re - Im*sF.Im;
         Im = Im*sF.Re + Re*sF.Im;
         Re = tmp;
         return complex(Re,Im);
     }
-    complex operator/=(const complex& sF){
+    complex complex::operator/=(const complex& sF){
         double tmp;
         tmp = (Re*sF.Re + Im*sF.Im)/(sF.Re*sF.Re + sF.Im*sF.Im);
         Im =  (-Re*sF.Im + Im*sF.Re)/(sF.Re*sF.Re + sF.Im*sF.Im);
         Re = tmp;
         return complex(Re,Im);
     }
-    complex operator=(const complex& sF){
+    complex complex::operator=(const complex& sF){
         Re = sF.Re;
         Im = sF.Im;
         return complex(Re,Im);
     }
-    bool operator==(const complex& sF){
+    bool complex::operator==(const complex& sF){
         return (Re == sF.Re && Im == sF.Im);
     } 
     
